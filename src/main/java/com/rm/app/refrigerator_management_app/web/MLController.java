41651c5d45aaa221cc9789b376739f91539c0771
soosungp33 <soosungp33@gmail.com>
2021-06-01 13:43:21 +0900

    connect flask

--- src/main/java/com/rm/app/refrigerator_management_app/web/MLController.java
+++ src/main/java/com/rm/app/refrigerator_management_app/web/MLController.java
@@ -1,12 +1,35 @@
 package com.rm.app.refrigerator_management_app.web;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MLController {
-    @RequestMapping(value = "/ml")
-    public String ml() {
-        return "ml";
+    String url = "http://210.117.181.74:5000/detection";
+
+    @GetMapping(value = "/ml")
+    @CrossOrigin("*")
+    public ResponseEntity<String> ml() {
+        // to Flask
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+
+        // make Entity
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange("http://210.117.181.74:5000/detection", HttpMethod.GET, entity, String.class);
+
+        return response;
     }
 }
