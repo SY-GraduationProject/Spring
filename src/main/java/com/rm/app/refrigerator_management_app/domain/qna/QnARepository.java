ffd3c4a0596f4b2c94f6b11b0279688a5a0b18d4
DESKTOP-1J72GLA\HanSangHyeon <luck2901@naver.com>
2021-06-03 17:16:17 +0900

    Update/QnA

---
+++ src/main/java/com/rm/app/refrigerator_management_app/domain/qna/QnARepository.java
@@ -0,0 +1,6 @@
+package com.rm.app.refrigerator_management_app.domain.qna;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnARepository extends JpaRepository<QnA, Long> {
+}
